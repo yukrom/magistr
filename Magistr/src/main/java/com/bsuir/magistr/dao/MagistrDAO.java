e48db5cd4ff2ceb001a2056137b6d8318a2e2888
yukrom <yukhnevich.roman@gmail.com>
2014-05-12 18:19:57 +0300

    commit this shit

--- Magistr/src/main/java/com/bsuir/magistr/dao/MagistrDAO.java
+++ Magistr/src/main/java/com/bsuir/magistr/dao/MagistrDAO.java
@@ -1,11 +1,12 @@
 package com.bsuir.magistr.dao;
 
 import com.bsuir.magistr.domain.Magistr;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by Roma on 07.05.2014.
  */
 
 public interface MagistrDAO {
-    public void addMagistr(Magistr magistr);
+    public void addMagistr(@Param("magistr") Magistr magistr);
 }
